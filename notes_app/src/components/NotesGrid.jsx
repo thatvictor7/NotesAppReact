292fcbdf34358a407ac964bc40fd4d249af72660
Victor Montoya <victormontoya@199.127.134.83.co-eaglenet.net>
2020-01-17 15:47:39 -0700

    added modal

--- notes_app/src/components/NotesGrid.jsx
+++ notes_app/src/components/NotesGrid.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import { Paper, Modal, Fade, Backdrop } from '@material-ui/core';
 import Note from './Note'
 
 const useStyles = makeStyles(theme => ({
@@ -29,17 +29,60 @@ const useStyles = makeStyles(theme => ({
   divider: {
     margin: theme.spacing(2, 0),
   },
+
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paperTest: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }
 ))
 
 const NotesGrid = (props) => {
 
   const { notesArray } = props
+  
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
   const classes = useStyles()
+
+  const popup = (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      className={classes.modal}
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <div className={classes.paperTest}>
+          <h2 id="transition-modal-title">Transition modal</h2>
+          <p id="transition-modal-description">react-transition-group animates me.</p>
+        </div>
+      </Fade>
+    </Modal>
+  )
+
   const notesDisplay = notesArray.map(note => {
     console.log(note)
-    
     return <div style={{ gridColumnEnd: 'span 3' }}>
+    {/* // return <div style={{ gridColumnEnd: 'span 3' }}> */}
       <Paper className={classes.paper}>
         <Note noteTitle={note.title}
               noteText={note.content}
@@ -53,21 +96,21 @@ const NotesGrid = (props) => {
       <div className={classes.container}>
       {notesDisplay}
 
-        <div style={{ gridColumnEnd: 'span 3' }}>
+        <div onClick={handleOpen} style={{ gridColumnEnd: 'span 3' }}>
           <Paper className={classes.paper}>
             <Note noteTitle={`note.title`}
               noteText={`nodldkfsdaf ldfsd khdfj dsfkjsdhf sdfkjs dfkdsdfkhskdf  sh  hfsdfksdhf kste.content`}
             />
           </Paper>
         </div>
-        <div style={{ gridColumnEnd: 'span 3' }}>
+        <div onClick={handleOpen} style={{ gridColumnEnd: 'span 3' }}>
           <Paper className={classes.paper}>
             <Note noteTitle={`note.title`}
               noteText={`dsfddslfksdjfjsdlfj dlk fksdjfksdfasd sf`}
             />
           </Paper>
         </div>
-
+        {popup}
       </div>
     </div>
   )
